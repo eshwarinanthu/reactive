98129b986bdc090b88a11d1f549423211517d046
eshwari <eshwarinanthu@gmail.com>
2022-01-19 19:58:12 +0530

    my first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rf';
+  submitted: any ;
+  registerForm: any;
+
+  register=new FormGroup({
+    title:new FormControl ('', Validators.required),//form validation
+    username:new FormControl('',Validators.required),
+    password:new FormControl('',[Validators.required, Validators.minLength(6)]),
+    email:new FormControl('',[Validators.required,Validators.required])
+  })
+  
+  get f() { return this.register.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.register.invalid) {
+        return;
+    }
+
+    // display form values on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.register.value, null, 4));
 }
+ 
+  onReset() {
+    this.submitted = false;
+    this.register.reset();
+}
+}
+
